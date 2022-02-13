45e9339265ec64a981e95f4b97c91d807b24410f
BriandaCampoy <brianda.pekachu@gmail.com>
2022-02-12 22:02:03 -0700

    advertencia agregada

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React LOOS ALIENS YA VIENEN
         </a>
       </header>
     </div>
